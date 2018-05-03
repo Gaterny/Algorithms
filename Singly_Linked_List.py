7a1958b42250dae3c13769c5e39dbd376289c2a2
Gaterny <scusong@foxmail.com>
2018-05-03 19:05:04 +0800

    Create Singly_Linked_List.py
    单链表

---
+++ Singly_Linked_List.py
@@ -0,0 +1,107 @@
+class Node(object):
+    """节点"""
+
+    def __init__(self, elem):
+        # item存放数据元素
+        self.elem = elem
+        # next是下一节点的标识
+        self.next = None
+
+
+class SingleList(object):
+    """单链表"""
+
+    def __init__(self, node=None):
+        self.__head = node
+
+    def is_empty(self):
+        """链表是否为空"""
+        return self.__head == None
+
+    def length(self):
+        """链表长度"""
+
+        # 游标用来移动遍历节点
+        cur = self.__head
+        # 节点数量计数
+        count = 0
+        while cur != None:
+            count += 1
+            cur = cur.next
+        return count
+
+    def travel(self):
+        """遍历整个链表"""
+        cur = self.__head
+        while cur != None:
+            print(cur.elem, end=" ")
+            cur = cur.next
+
+    def add(self, item):
+        """链表头部添加元素"""
+        node = Node(item)
+        node.next = self.__head
+        self.__head = node
+
+    def append(self, item):
+        """链表尾部添加元素"""
+        node = Node(item)
+        if self.is_empty():
+            self.__head = node
+        else:
+            cur = self.__head
+            while cur.next != None:
+                cur = cur.next
+            cur.next = node
+
+    def insert(self, pos, item):
+        """指定位置插入元素"""
+        if pos < 0:
+            self.add(item)
+        elif pos > (self.length() - 1):
+            self.append(item)
+        else:
+            pre = self.__head
+            count = 0
+            while count < (pos - 1):
+                count += 1
+                pre = pre.next
+            node = Node(item)
+            node.next = pre.next
+            pre.next = node
+
+    def remove(self, item):
+        """删除节点"""
+        cur = self.__head
+        pre = None
+        while cur != None:
+            if cur.elem == item:
+                # 判断是否是头结点
+                if cur == self.__head:
+                    self.__head = cur.next
+                else:
+                    pre.next = cur.next
+                break
+            else:
+                pre = cur
+                cur = cur.next
+
+    def search(self, item):
+        """查找节点是否存在"""
+        cur = self.__head
+        while cur != None:
+            if cur.elem == item:
+                return True
+            else:
+                cur = cur.next
+        return False
+
+
+if __name__ == "__main__":
+    l = SingleList()
+    l.append(10)
+    l.add(12)
+    l.insert(-1, 3)
+    l.remove(12)
+    print(l.is_empty(), l.length())
+    l.travel()
